a5eee96c9bdd1d2aaa7a5e68001098759e577729
sergiolimajr <devsergiojr@gmail.com>
2018-09-09 13:49:26 -0300

    distance em js

--- js/distance.js
+++ js/distance.js
@@ -5,53 +5,59 @@ class Selecao {
  }
 }
 
-var menorValor = new Selecao(null,999999);
-console.log(menorValor);
+var menorValor = new Selecao(null, 999999);
 
 var arrayDistances = [];
+let btnSearch = document.getElementById('getDistance');
 
-$('#getDistance').click(function () {
-  $('#litResultado').html('Aguarde...');
-  //Instanciar o DistanceMatrixService
-  var service = new google.maps.DistanceMatrixService();
-  //executar o DistanceMatrixService
+btnSearch.addEventListener('click', function() {
+  let origem = document.getElementById('origem');
+  let destino = document.getElementById('destino');
+  sResult = document.getElementById('search-result');
+
+  sResult.innerHTML = 'Aguarde...';
+
+  // Instanciar o DistanceMatrixService
+  let service = new google.maps.DistanceMatrixService();
+  
+  // Executar o DistanceMatrixService
   service.getDistanceMatrix({
-    //Origem
-    origins: [$("#txtOrigem").val()],
-    //Destino
-    destinations: [$("#txtDestino").val()],
-    //Modo Carro
+    origins: [origem.value],
+    destinations: [destino.value],
     travelMode: google.maps.TravelMode.DRIVING,
-    //Sistema de medida (METRIC | IMPERIAL)
     unitSystem: google.maps.UnitSystem.METRIC
-    //Vai chamar a função tratarRetorno
   }, tratarRetorno);
 });
 
 function tratarRetorno(response, status) {
-//Verificar o Status
-if (status != google.maps.DistanceMatrixStatus.OK)
+  let map = document.getElementById('map');
+
+  //Verificar o Status
+  if (status != google.maps.DistanceMatrixStatus.OK) {
+
     //Se o status não for "OK"
-  $('#litResultado').html(status);
-  else {                  
+    sResult.innerHTML = status;
+  } else {                  
     //Imprimindo o Retorno JSON na tela
-    $('#litResultado').html("<strong>Origem:</strong> " + response.originAddresses +
-      "<br /><strong>Destino:</strong> " + response.destinationAddresses +
-      "<br /><strong>Distância:</strong> " + response.rows[0].elements[0].distance.text +
-      " <br /><strong>Duração:</strong> " + response.rows[0].elements[0].duration.text +
-      "<br /><strong>Tipo de transporte: </strong>Carro " 
-      );                
-  //Atualizando o mapa
-  $("#map").attr("src", "https://maps.google.com/maps?saddr=" + response.originAddresses + "&daddr=" + response.destinationAddresses + "&output=embed");}
-  if(status === google.maps.DistanceMatrixStatus.OK){
+    sResult.innerHTML = ` <b>Origem:</b> ${response.originAddresses} 
+                          <br /><b>Destino:</b> ${response.destinationAddresses}
+                          <br /><b>Distância:</b> ${response.rows[0].elements[0].distance.text}
+                          <br /><b>Duração:</b> ${response.rows[0].elements[0].duration.text}
+                          <br /><b>Tipo de Transporte:</b> Carro`;
+
+    //Atualizando o mapa
+    map.src = `https://maps.google.com/maps?saddr=${response.originAddresses}&daddr=${response.destinationAddresses}&output=embed`;
+  }
+
+  if(status === google.maps.DistanceMatrixStatus.OK) {
     arrayDistances.push(response.rows[0].elements[0].distance.text);
-    if (response.rows[0].elements[0].distance.value < menorValor.menor){
+
+    if (response.rows[0].elements[0].distance.value < menorValor.menor) {
      menorValor.destino = response.destinationAddresses;
      menorValor.menorValor = response.rows[0].elements[0].distance.value;
    } 
-   console.log(menorValor);
  }
   /*for (var i = 0; i <= arrayDistances.length; i++) {
       console.log(arrayDistances[i]);
     }*/
-  }
+}
